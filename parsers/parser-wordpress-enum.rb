6392b91911d071fd1fab513c4d7061f04fa0110e
sneakerhax <sneakerhax@gmail.com>
2016-03-29 22:06:29 -0700

    Updated parser-wordpress-enum to add port 443

--- parsers/parser-wordpress-enum.rb
+++ parsers/parser-wordpress-enum.rb
@@ -7,7 +7,10 @@ Nmap::XML.new(report) do |xml|
     puts "[#{host.ip}]"
 
     host.each_port do |port|
-      puts " Port: #{port.number}/#{port.protocol}\t State: #{port.state}\t Service: #{port.service}\t Wordpress-enum: #{port.scripts.values[1]}"
+      ports = [80,443]
+        if ports.include?(port.number)
+          puts " Port: #{port.number}/#{port.protocol}\t State: #{port.state}\t Service: #{port.service}\t Wordpress-enum: #{port.scripts.values[1]}"
+      end
     end
   end
 end
